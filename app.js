70ad9a1983ad5ea6fe453dad3003a4770cb7b81b
SyedMuhammadTalhaHasan <155646073+SyedMuhammadTalhaHasan@users.noreply.github.com>
2024-01-25 09:20:09 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+firstbtn.addEventListener("click", function() {
+    document.getElementById("heading").innerHTML="INTRODUCTION!!"
+})
+secondbtn.addEventListener("click", function() {
+    document.getElementById("heading").innerHTML=""
+})
+
+thirdbtn.addEventListener("click", function() {
+    document.getElementById("para").innerHTML="My name is SYED MUHAMMAD TALHA HASAN. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ipsam, mollitia natus repellat quia officiis hic atque, voluptates aut deserunt necessitatibus numquam sit. Odio eveniet sint eligendi maxime earum ut? Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati sapiente temporibus, culpa libero modi. Molestias ut incidunt minus cumque itaque provident laudantium, veniam labore dolorum. Deserunt voluptatibus magni illum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit consectetur saepe, eaque tempore sapiente ab non mollitia rerum quod dolor totam laborum. Velit tempora doloribus, distinctio consequuntur sapiente quas molestiae."
+});
+fourthbtn.addEventListener("click", function() {
+    document.getElementById("para").innerHTML=""
+})
+
+fifthbtn.addEventListener("click", function() {
+    document.getElementById("heading").style.color = "#ffbd39";
+    document.getElementById("heading").style.backgroundColor = "black";
+    document.getElementById("heading").style.borderColor = "black";
+    document.getElementById("heading").style.margin = "5px";
+    document.getElementById("heading").style.padding = "15px 0px";
+    document.getElementById("heading").style.width = "80%";
+    document.getElementById("heading").style.letterSpacing = "5px"
+    document.getElementById("heading").style.fontStyle = "italic"
+})
+sixthbtn.addEventListener("click", function() {
+    document.getElementById("heading").style.color = "";
+    document.getElementById("heading").style.backgroundColor = "";
+    document.getElementById("heading").style.borderColor = "";
+    document.getElementById("heading").style.margin = "";
+    document.getElementById("heading").style.padding = "";
+    document.getElementById("heading").style.width = "";
+    document.getElementById("heading").style.letterSpacing = ""
+    document.getElementById("heading").style.fontStyle = ""
+})
+
+seventhbtn.addEventListener("click", function() {
+    document.getElementById("para").style.color = "white";
+    document.getElementById("para").style.backgroundColor = "black";
+    document.getElementById("para").style.borderColor = "black";
+    document.getElementById("para").style.margin = "5px";
+    document.getElementById("para").style.padding = "15px 0px";
+    document.getElementById("para").style.width = "80%";
+    document.getElementById("para").style.wordSpacing = "3px"
+})
+eighthbtn.addEventListener("click", function() {
+    document.getElementById("para").style.color = "";
+    document.getElementById("para").style.backgroundColor = "";
+    document.getElementById("para").style.borderColor = "";
+    document.getElementById("para").style.margin = "";
+    document.getElementById("para").style.padding = "";
+    document.getElementById("para").style.width = "";
+    document.getElementById("para").style.wordSpacing = ""
+})
+
+ninethbtn.addEventListener("click", function() {
+    document.getElementById("mdiv").style.backgroundColor = "black";
+    document.getElementById("mdiv").style.borderColor = "black";
+    document.getElementById("mdiv").style.margin = "5px";
+    document.getElementById("mdiv").style.padding = "15px 0px";
+    document.getElementById("mdiv").style.width = "80%";
+    document.getElementById("mdiv").style.boxShadow = "5px 5px rgb(204, 200, 200)"
+    document.getElementById("mdiv").style.borderRadius = "5px";
+})
+tenthbtn.addEventListener("click", function() {
+    document.getElementById("mdiv").style.backgroundColor = "";
+    document.getElementById("mdiv").style.borderColor = "";
+    document.getElementById("mdiv").style.margin = "";
+    document.getElementById("mdiv").style.padding = "";
+    document.getElementById("mdiv").style.width = "";
+    document.getElementById("mdiv").style.boxShadow = ""
+    document.getElementById("mdiv").style.borderRadius = "";
+})
